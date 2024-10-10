56bec93b18c79865dfb90e229308e4603b47eea3
dfsanghyuk12 <dfsanghyuk12@naver.com>
2024-10-10 12:43:24 +0900

    5주차 실습파일

--- 4week/ex4.py
+++ 4week/ex4.py
@@ -7,3 +7,12 @@ a = int(input())
 result = sumfunc(a)
 print(f'1~ {a}까지의 합은 {result}이다')
 
+
+
+#def sumfunc(num):
+#  sum = 0
+#  for j in range(1,num+1):
+#  sum = sum + j
+#  return sum
+#sum = sumfunc(num)
+#print(f"1~{num}까지 정수의 합은 {sum}입니다)
